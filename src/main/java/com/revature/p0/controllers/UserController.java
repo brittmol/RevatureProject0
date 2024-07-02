b039b3bf8ff22655354a7b13b2b6fe6c6b683607
Brittany Moliver <britt.moliver@gmail.com>
2024-07-02 13:19:32 -0400

    added controllers

---
+++ src/main/java/com/revature/p0/controllers/UserController.java
@@ -0,0 +1,11 @@
+package com.revature.p0.controllers;
+
+import com.revature.p0.services.UserService;
+
+public class UserController {
+    UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+}
