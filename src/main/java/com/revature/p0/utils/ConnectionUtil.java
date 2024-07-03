4891e4d87cf8850b610829edd9ce888f2b37e648
Brittany Moliver <britt.moliver@gmail.com>
2024-07-03 10:36:05 -0400

    connectionutil

--- src/main/java/com/revature/p0/utils/ConnectionUtil.java
+++ src/main/java/com/revature/p0/utils/ConnectionUtil.java
@@ -1,12 +1,20 @@
 package com.revature.p0.utils;
 
-import java.io.*;
-import java.sql.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
 
 public class ConnectionUtil {
-    public static void main(String[] args) throws SQLException, IOException {
+    public static Connection getConnection() throws SQLException, IOException {
+        InputStream inputStream = ConnectionUtil.class.getClassLoader().getResourceAsStream("application.properties");
+        Properties props = new Properties();
+        props.load(inputStream);
 
+        Connection conn = DriverManager.getConnection(props.getProperty("url"), props.getProperty("username"), props.getProperty("password"));
 
+        return conn;
     }
-
 }
