f50dbeae62dcb6a3f2ae8cc9bf0f810c5617ea27
Brittany Moliver <britt.moliver@gmail.com>
2024-07-03 10:24:15 -0400

    properties

--- src/main/java/com/revature/p0/utils/ConnectionUtil.java
+++ src/main/java/com/revature/p0/utils/ConnectionUtil.java
@@ -1,5 +1,12 @@
 package com.revature.p0.utils;
 
+import java.io.*;
+import java.sql.*;
+
 public class ConnectionUtil {
+    public static void main(String[] args) throws SQLException, IOException {
+
+
+    }
 
 }
